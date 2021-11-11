45e87b6e1efb02b0f5c335949546b8ae8805c8c9
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2021-11-11 19:18:46 +0700

    10038 - Jolly Jumpers

---
+++ src/main/java/dev/rizaldi/uhunt/p10038/README.md
@@ -0,0 +1,3 @@
+# 10038 - Jolly Jumpers
+- 2\. Data Structures and Libraries > 1D Array Manipulation, Medium > 10038 - Jolly Jumpers
+- https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=979
---
+++ src/test/java/dev/rizaldi/uhunt/p10038/MainTests.java
@@ -0,0 +1,36 @@
+package dev.rizaldi.uhunt.p10038;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
+
+public class MainTests {
+
+    @Test
+    public void example() throws Exception {
+        int totalTest = 1;
+        for (int testcase = 1; testcase <= totalTest; testcase++) {
+            String inputPath = String.format("/p10038/%d.in", testcase);
+            InputStream inputStream = this.getClass().getResourceAsStream(inputPath);
+            System.setIn(inputStream);
+
+            String outputPath = String.format("/p10038/%d.out", testcase);
+            InputStream outputStream = this.getClass().getResourceAsStream(outputPath);
+            String output = IOUtils.toString(outputStream, Charset.defaultCharset());
+
+            ByteArrayOutputStream actualOutputStream = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(actualOutputStream));
+
+            Main.main();
+
+            String actualOutput = actualOutputStream.toString();
+
+            Assertions.assertEquals(output, actualOutput);
+        }
+    }
+}
