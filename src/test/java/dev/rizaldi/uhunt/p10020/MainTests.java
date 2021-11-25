d8e2e55004e2ce2fa61098a388046f33d809e6f1
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2021-11-25 18:36:00 +0700

    [AC] 10020 - Minimal coverage

---
+++ src/test/java/dev/rizaldi/uhunt/p10020/MainTests.java
@@ -0,0 +1,25 @@
+package dev.rizaldi.uhunt.p10020;
+
+import dev.rizaldi.uhunt.helper.TestFileHelper;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+public class MainTests {
+    private final File testDirectory = TestFileHelper.getTestDirectory(getClass());
+
+    @Test
+    public void sample() throws Exception {
+        TestFileHelper.runSingleTest(testDirectory, "sample", Main::main);
+    }
+
+    @Test
+    public void morass() throws Exception {
+        TestFileHelper.runSingleTest(testDirectory, "morass", Main::main);
+    }
+
+    @Test
+    public void brianfry713() throws Exception {
+        TestFileHelper.runSingleTest(testDirectory, "brianfry713", Main::main);
+    }
+}
