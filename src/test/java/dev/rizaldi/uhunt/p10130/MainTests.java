480d5898d338edaf9d86668883084ef796a4df3a
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2021-11-26 19:36:57 +0700

    [AC] 10130 - SuperSale

---
+++ src/test/java/dev/rizaldi/uhunt/p10130/MainTests.java
@@ -0,0 +1,15 @@
+package dev.rizaldi.uhunt.p10130;
+
+import dev.rizaldi.uhunt.helper.TestFileHelper;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+public class MainTests {
+    private final File testDirectory = TestFileHelper.getTestDirectory(getClass());
+
+    @Test
+    public void sample() throws Exception {
+        TestFileHelper.runSingleTest(testDirectory, "sample", Main::main);
+    }
+}
