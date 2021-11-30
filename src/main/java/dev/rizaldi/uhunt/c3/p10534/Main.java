b399811acf489eed2083c207392389215062dcc5
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2021-11-30 14:58:37 +0700

    [AC] 10534 - Wavio Sequence

---
+++ src/main/java/dev/rizaldi/uhunt/c3/p10534/Main.java
@@ -0,0 +1,79 @@
+package dev.rizaldi.uhunt.c3.p10534;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String... args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()) {
+            int total = in.nextInt();
+            int[] values = new int[total];
+            for (int i = 0; i < total; i++) values[i] = in.nextInt();
+
+            Solution solution = new Solution(total, values);
+            int longestSequence = solution.getLongestWavioSequence();
+
+            System.out.println(longestSequence);
+        }
+    }
+}
+
+class Solution {
+    private final int total;
+    private final int[] values;
+
+    public Solution(int total, int[] values) {
+        this.total = total;
+        this.values = values;
+    }
+
+    public int getLongestWavioSequence() {
+        int[] lis = calculateLIS(values);
+        int[] lds = calculateLDS(values);
+
+        int maxLength = 0;
+        for (int i = 0; i < total; i++) {
+            int increase = lis[i];
+            int decrease = lds[i];
+            int halfLength = Math.min(increase, decrease);
+
+            int length = halfLength + halfLength - 1;
+            maxLength = Math.max(maxLength, length);
+        }
+
+        return maxLength;
+    }
+
+    private int[] calculateLDS(int[] values) {
+        return reverseValues(calculateLIS(reverseValues(values)));
+    }
+
+    private int[] calculateLIS(int[] values) {
+        int[] lisLength = new int[values.length];
+        ArrayList<Integer> lisSequence = new ArrayList<>(values.length);
+
+        for (int i = 0; i < values.length; i++) {
+            int value = values[i];
+
+            int replaceID = lisSequence.size();
+            while (replaceID > 0) {
+                if (value <= lisSequence.get(replaceID - 1)) replaceID--;
+                else break;
+            }
+
+            if (replaceID == lisSequence.size()) lisSequence.add(value);
+            else lisSequence.set(replaceID, value);
+
+            lisLength[i] = lisSequence.size();
+        }
+
+        return lisLength;
+    }
+
+    private int[] reverseValues(int[] values) {
+        int[] reverseValues = new int[values.length];
+        for (int i = 0, j = values.length - 1; i < values.length; i++, j--) reverseValues[i] = values[j];
+        return reverseValues;
+    }
+}
---
+++ src/test/java/dev/rizaldi/uhunt/c3/p10534/MainTests.java
@@ -0,0 +1,30 @@
+package dev.rizaldi.uhunt.c3.p10534;
+
+import dev.rizaldi.uhunt.helper.TestFileHelper;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+public class MainTests {
+    private final File testDirectory = TestFileHelper.getTestDirectory(getClass());
+
+    @Test
+    public void sample() throws Exception {
+        TestFileHelper.runSingleTest(testDirectory, "sample", Main::main);
+    }
+
+    @Test
+    public void uva() throws Exception {
+        TestFileHelper.runSingleTest(testDirectory, "uva", Main::main);
+    }
+
+    @Test
+    public void batman() throws Exception {
+        TestFileHelper.runSingleTest(testDirectory, "batman", Main::main);
+    }
+
+    @Test
+    public void rizaldi() throws Exception {
+        TestFileHelper.runSingleTest(testDirectory, "rizaldi", Main::main);
+    }
+}
