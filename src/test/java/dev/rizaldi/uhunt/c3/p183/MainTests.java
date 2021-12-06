8baa3d9e2f3bbe71d144ef4cb2a65fb934c4e832
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2021-12-06 12:27:16 +0700

    [AC] 183 - Bit Maps

--- src/test/java/dev/rizaldi/uhunt/c3/p183/MainTests.java
+++ src/test/java/dev/rizaldi/uhunt/c3/p183/MainTests.java
@@ -2,6 +2,7 @@ package dev.rizaldi.uhunt.c3.p183;
 
 import dev.rizaldi.uhunt.helper.TestFileHelper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import java.io.File;
 
@@ -9,16 +10,19 @@ public class MainTests {
     private final File testDirectory = TestFileHelper.getTestDirectory(getClass());
 
     @Test
+    @Timeout(3)
     public void sample() throws Exception {
         TestFileHelper.runSingleTest(testDirectory, "sample", Main::main);
     }
 
     @Test
+    @Timeout(3)
     public void morass1() throws Exception {
         TestFileHelper.runSingleTest(testDirectory, "morass_1", Main::main);
     }
 
     @Test
+    @Timeout(3)
     public void morass2() throws Exception {
         TestFileHelper.runSingleTest(testDirectory, "morass_2", Main::main);
     }
