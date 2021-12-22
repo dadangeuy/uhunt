f4c60bd94a582a5fc6f1209a83a2a2910aa7dde5
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2021-12-22 16:56:03 +0700

    [AC] 713 - Adding Reversed Numbers

---
+++ src/test/java/dev/rizaldi/uhunt/c2/p713/MainTests.java
@@ -0,0 +1,23 @@
+package dev.rizaldi.uhunt.c2.p713;
+
+import dev.rizaldi.uhunt.helper.TestFileHelper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.File;
+
+public class MainTests {
+    private final File directory = TestFileHelper.getTestDirectory(getClass());
+
+    @Test
+    @Timeout(3)
+    public void sample() throws Exception {
+        TestFileHelper.runSingleTest(directory, "sample", Main::main);
+    }
+
+    @Test
+    @Timeout(3)
+    public void udebug() throws Exception {
+        TestFileHelper.runSingleTest(directory, "udebug", Main::main);
+    }
+}
