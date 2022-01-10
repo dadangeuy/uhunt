30d78589d80ebf09c1ff534f6ef0b15e18dfd767
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2022-01-10 15:37:47 +0700

    [AC] 10765 - Doves and bombs

---
+++ src/test/java/dev/rizaldi/uhunt/c4/p10765/MainTests.java
@@ -0,0 +1,40 @@
+package dev.rizaldi.uhunt.c4.p10765;
+
+import dev.rizaldi.uhunt.helper.TestHelper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.File;
+
+public class MainTests {
+    private final File directory = TestHelper.getDirectory(getClass());
+
+    @Test
+    @Timeout(3)
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, directory, "sample");
+    }
+
+    @Test
+    @Timeout(3)
+    public void anjupiter() throws Exception {
+        TestHelper.run(Main::main, directory, "anjupiter");
+    }
+
+    @Test
+    @Timeout(3)
+    public void fer22f_1() throws Exception {
+        TestHelper.run(Main::main, directory, "fer22f_1");
+    }
+
+    @Test
+    @Timeout(3)
+    public void fer22f_2() throws Exception {
+        TestHelper.run(Main::main, directory, "fer22f_2");
+    }
+
+    @Test
+    public void rizaldi() throws Exception {
+        TestHelper.run(Main::main, directory, "rizaldi");
+    }
+}
