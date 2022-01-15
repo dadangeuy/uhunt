608c9762db0ec1d3fd1ea6af7c2586a7a239e239
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2022-01-15 16:24:29 +0700

    [AC] 10646 - What is the Card?

---
+++ src/test/java/dev/rizaldi/uhunt/c1/p10646/MainTests.java
@@ -0,0 +1,41 @@
+package dev.rizaldi.uhunt.c1.p10646;
+
+import dev.rizaldi.uhunt.helper.TestHelper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.File;
+
+public class MainTests {
+    private final File directory = TestHelper.getDirectory(getClass());
+
+    @Test
+    @Timeout(3)
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, directory, "sample");
+    }
+
+    @Test
+    @Timeout(3)
+    public void uva() throws Exception {
+        TestHelper.run(Main::main, directory, "uva");
+    }
+
+    @Test
+    @Timeout(3)
+    public void morass_1() throws Exception {
+        TestHelper.run(Main::main, directory, "morass_1");
+    }
+
+    @Test
+    @Timeout(3)
+    public void morass_2() throws Exception {
+        TestHelper.run(Main::main, directory, "morass_2");
+    }
+
+    @Test
+    @Timeout(3)
+    public void the_madman() throws Exception {
+        TestHelper.run(Main::main, directory, "the_madman");
+    }
+}
