7196d7557d3d36d96b37ee4e200277177d8cb14a
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2022-01-24 22:25:23 +0700

    [AC] 11547 - Automatic Answer

---
+++ src/main/java/dev/rizaldi/uhunt/c1/p11547/Main.java
@@ -0,0 +1,24 @@
+package dev.rizaldi.uhunt.c1.p11547;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String... args) {
+        Scanner in = new Scanner(new BufferedInputStream(System.in, 1 << 16));
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out, 1 << 16));
+
+        int totalCase = in.nextInt();
+        for (int i = 0; i < totalCase; i++) {
+            int n = in.nextInt();
+            int ans = (((((n * 567) / 9) + 7492) * 235) / 47) - 498;
+            out.println(Math.abs(ans % 100 - ans % 10) / 10);
+        }
+
+        in.close();
+        out.flush();
+        out.close();
+    }
+}
