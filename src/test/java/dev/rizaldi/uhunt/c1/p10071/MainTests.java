7326c7f004f750c07b6ded7e5572e68a1247fc35
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2022-01-24 21:43:25 +0700

    [AC] 10071 - Back to High School Physics

---
+++ src/test/java/dev/rizaldi/uhunt/c1/p10071/MainTests.java
@@ -0,0 +1,23 @@
+package dev.rizaldi.uhunt.c1.p10071;
+
+import dev.rizaldi.uhunt.helper.TestHelper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.File;
+
+public class MainTests {
+    private final File directory = TestHelper.getDirectory(getClass());
+
+    @Test
+    @Timeout(3)
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, directory, "sample");
+    }
+
+    @Test
+    @Timeout(3)
+    public void udebug() throws Exception {
+        TestHelper.run(Main::main, directory, "udebug");
+    }
+}
