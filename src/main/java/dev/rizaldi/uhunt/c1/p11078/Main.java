eff8c5d2823f64548388cf5f983b8d1712a77b21
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2022-03-21 00:14:53 +0700

    [AC] 11078 - Open Credit System

---
+++ src/main/java/dev/rizaldi/uhunt/c1/p11078/Main.java
@@ -0,0 +1,51 @@
+package dev.rizaldi.uhunt.c1.p11078;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String... args) {
+        Scanner in = new Scanner(new BufferedInputStream(System.in, 1 << 16));
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out, 1 << 16));
+
+        int totalCase = in.nextInt();
+        for (int i = 0; i < totalCase; i++) {
+            int totalStudent = in.nextInt();
+            int[] scores = new int[totalStudent];
+            for (int j = 0; j < totalStudent; j++) scores[j] = in.nextInt();
+
+            Solution solution = new Solution(scores);
+            int difference = solution.getMaximumDifferenceBetweenSeniorAndJunior();
+
+            out.println(difference);
+        }
+
+        in.close();
+        out.flush();
+        out.close();
+    }
+}
+
+class Solution {
+    private final int[] scores;
+
+    public Solution(int[] scores) {
+        this.scores = scores;
+    }
+
+    public int getMaximumDifferenceBetweenSeniorAndJunior() {
+        int maxDifference = scores[0] - scores[1];
+        int maxScore = scores[0];
+        for (int i = 1; i < scores.length; i++) {
+            int difference = maxScore - scores[i];
+            maxDifference = Math.max(maxDifference, difference);
+
+            maxScore = Math.max(maxScore, scores[i]);
+        }
+
+
+        return maxDifference;
+    }
+}
