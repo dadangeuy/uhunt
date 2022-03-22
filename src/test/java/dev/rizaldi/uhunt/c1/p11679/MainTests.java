50ec7bc079de97787da00a26b85215d50f276622
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2022-03-22 19:57:38 +0700

    [AC] 11679 - Sub-prime

---
+++ src/test/java/dev/rizaldi/uhunt/c1/p11679/MainTests.java
@@ -0,0 +1,23 @@
+package dev.rizaldi.uhunt.c1.p11679;
+
+import dev.rizaldi.uhunt.helper.TestHelper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.File;
+
+public class MainTests {
+    private final File directory = TestHelper.getDirectory(getClass());
+
+    @Test
+    @Timeout(1)
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, directory, "sample");
+    }
+
+    @Test
+    @Timeout(1)
+    public void morass() throws Exception {
+        TestHelper.run(Main::main, directory, "morass");
+    }
+}
