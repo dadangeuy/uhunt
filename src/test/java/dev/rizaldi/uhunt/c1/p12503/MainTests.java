50376077c9597d734ce7c1322e94c6861b0ef6a6
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2022-03-23 10:37:29 +0700

    [AC] 12503 - Robot Instructions

---
+++ src/test/java/dev/rizaldi/uhunt/c1/p12503/MainTests.java
@@ -0,0 +1,23 @@
+package dev.rizaldi.uhunt.c1.p12503;
+
+import dev.rizaldi.uhunt.helper.TestHelper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.File;
+
+public class MainTests {
+    private final File directory = TestHelper.getDirectory(getClass());
+
+    @Test
+    @Timeout(1)
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, directory, "sample");
+    }
+
+    @Test
+    @Timeout(1)
+    public void brianfry713() throws Exception {
+        TestHelper.run(Main::main, directory, "brianfry713");
+    }
+}
