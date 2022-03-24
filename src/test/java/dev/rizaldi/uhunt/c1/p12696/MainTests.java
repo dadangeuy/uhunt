20b52de100bcea4654c751dc472ddb7a771f6656
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2022-03-24 19:48:59 +0700

    [AC] 12696 - Cabin Baggage

---
+++ src/test/java/dev/rizaldi/uhunt/c1/p12696/MainTests.java
@@ -0,0 +1,17 @@
+package dev.rizaldi.uhunt.c1.p12696;
+
+import dev.rizaldi.uhunt.helper.TestHelper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.File;
+
+public class MainTests {
+    private final File directory = TestHelper.getDirectory(getClass());
+
+    @Test
+    @Timeout(1)
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, directory, "sample");
+    }
+}
