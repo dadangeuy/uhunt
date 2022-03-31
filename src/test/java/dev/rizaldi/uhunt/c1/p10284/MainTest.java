35795e5953e6ee33119aacad3f66f3539ddb7a08
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2022-03-31 21:11:26 +0700

    [AC] 10284 - Chessboard in FEN

---
+++ src/test/java/dev/rizaldi/uhunt/c1/p10284/MainTest.java
@@ -0,0 +1,35 @@
+package dev.rizaldi.uhunt.c1.p10284;
+
+import dev.rizaldi.uhunt.helper.TestHelper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.File;
+
+public class MainTest {
+    private final File directory = TestHelper.getDirectory(getClass());
+
+    @Test
+    @Timeout(3)
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, directory, "sample");
+    }
+
+    @Test
+    @Timeout(3)
+    public void dibery() throws Exception {
+        TestHelper.run(Main::main, directory, "dibery");
+    }
+
+    @Test
+    @Timeout(3)
+    public void ekangas() throws Exception {
+        TestHelper.run(Main::main, directory, "ekangas");
+    }
+
+    @Test
+    @Timeout(3)
+    public void clx() throws Exception {
+        TestHelper.run(Main::main, directory, "sample");
+    }
+}
