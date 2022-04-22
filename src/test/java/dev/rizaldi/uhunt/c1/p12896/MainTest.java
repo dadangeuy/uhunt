b9ca40a0d8e0dd76ba2c76ea798e765de7695a62
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2022-04-22 14:44:07 +0700

    [AC] 12896 - Mobile SMS

---
+++ src/test/java/dev/rizaldi/uhunt/c1/p12896/MainTest.java
@@ -0,0 +1,29 @@
+package dev.rizaldi.uhunt.c1.p12896;
+
+import dev.rizaldi.uhunt.helper.TestHelper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.File;
+
+public class MainTest {
+    private final File directory = TestHelper.getDirectory(getClass());
+
+    @Test
+    @Timeout(1)
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, directory, "sample");
+    }
+
+    @Test
+    @Timeout(1)
+    public void monir004() throws Exception {
+        TestHelper.run(Main::main, directory, "monir004");
+    }
+
+    @Test
+    @Timeout(1)
+    public void morass() throws Exception {
+        TestHelper.run(Main::main, directory, "morass");
+    }
+}
