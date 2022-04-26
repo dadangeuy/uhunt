d7e0dd7a123a8812d8aaf88d5b4ef633cbcf1dae
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2022-04-26 14:35:54 +0700

    [AC] 10906 - Strange Integration

---
+++ src/test/java/dev/rizaldi/uhunt/c1/p10906/MainTest.java
@@ -0,0 +1,29 @@
+package dev.rizaldi.uhunt.c1.p10906;
+
+import dev.rizaldi.uhunt.helper.TestHelper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.File;
+
+public class MainTest {
+    private final File directory = TestHelper.getDirectory(getClass());
+
+    @Test
+    @Timeout(3)
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, directory, "sample");
+    }
+
+    @Test
+    @Timeout(3)
+    public void uva() throws Exception {
+        TestHelper.run(Main::main, directory, "uva");
+    }
+
+    @Test
+    @Timeout(3)
+    public void txomin2091() throws Exception {
+        TestHelper.run(Main::main, directory, "txomin2091");
+    }
+}
