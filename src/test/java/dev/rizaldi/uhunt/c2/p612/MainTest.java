cc991b32cca935ee9c5430d69bf62eec0e1e12f0
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2022-08-17 11:40:13 +0700

    [AC] 612 - DNA Sorting

---
+++ src/test/java/dev/rizaldi/uhunt/c2/p612/MainTest.java
@@ -0,0 +1,25 @@
+package dev.rizaldi.uhunt.c2.p612;
+
+import dev.rizaldi.uhunt.helper.TestHelper;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+public class MainTest {
+    private final File directory = TestHelper.getDirectory(getClass());
+
+    @Test
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, directory, "sample");
+    }
+
+    @Test
+    public void uva() throws Exception {
+        TestHelper.run(Main::main, directory, "uva");
+    }
+
+    @Test
+    public void zeronfinity() throws Exception {
+        TestHelper.run(Main::main, directory, "zeronfinity");
+    }
+}
