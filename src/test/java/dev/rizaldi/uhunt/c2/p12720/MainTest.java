bb28a6ea34dea02e0e27600fe4d66e4854610d80
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2022-09-25 21:51:46 +0700

    [AC] 12720 - Algorithm of Phil

---
+++ src/test/java/dev/rizaldi/uhunt/c2/p12720/MainTest.java
@@ -0,0 +1,23 @@
+package dev.rizaldi.uhunt.c2.p12720;
+
+import dev.rizaldi.uhunt.helper.TestHelper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.File;
+
+public class MainTest {
+    private final File directory = TestHelper.getDirectory(getClass());
+
+    @Test
+    @Timeout(1)
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, directory, "sample");
+    }
+
+    @Test
+    @Timeout(1)
+    public void bryton() throws Exception {
+        TestHelper.run(Main::main, directory, "bryton");
+    }
+}
