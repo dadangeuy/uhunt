8f7c65a196feb2ae0017c649b331e39c568362d8
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2022-09-27 05:08:36 +0700

    [AC] 405 - Message Routing

---
+++ src/test/java/dev/rizaldi/uhunt/c1/p405/MainTest.java
@@ -0,0 +1,23 @@
+package dev.rizaldi.uhunt.c1.p405;
+
+import dev.rizaldi.uhunt.helper.TestHelper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.File;
+
+public class MainTest {
+    private final File directory = TestHelper.getDirectory(getClass());
+
+    @Test
+    @Timeout(3)
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, directory, "sample");
+    }
+
+    @Test
+    @Timeout(3)
+    public void brianfry713() throws Exception {
+        TestHelper.run(Main::main, directory, "brianfry713");
+    }
+}
