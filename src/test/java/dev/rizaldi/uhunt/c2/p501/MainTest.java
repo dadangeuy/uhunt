a85468970bd442143fbd510096995933b2436cee
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2025-08-19 20:00:21 +0700

    [AC] 501 - Black Box

---
+++ src/test/java/dev/rizaldi/uhunt/c2/p501/MainTest.java
@@ -0,0 +1,29 @@
+package dev.rizaldi.uhunt.c2.p501;
+
+import dev.rizaldi.uhunt.helper.TestHelper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.File;
+
+public class MainTest {
+    private final File testDirectory = TestHelper.getDirectory(getClass());
+
+    @Test
+    @Timeout(3)
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, testDirectory, "sample");
+    }
+
+    @Test
+    @Timeout(3)
+    public void morass() throws Exception {
+        TestHelper.run(Main::main, testDirectory, "morass");
+    }
+
+    @Test
+    @Timeout(3)
+    public void udebug() throws Exception {
+        TestHelper.run(Main::main, testDirectory, "udebug");
+    }
+}
