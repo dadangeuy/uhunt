b2e5bac5b1b1541c6355b3fbd0290fad3c61d26d
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2025-09-06 10:22:25 +0700

    [AC] 1193 - Radar Installation

---
+++ src/test/java/dev/rizaldi/uhunt/c3/p1193/MainTest.java
@@ -0,0 +1,35 @@
+package dev.rizaldi.uhunt.c3.p1193;
+
+import dev.rizaldi.uhunt.helper.TestHelper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.File;
+
+public class MainTest {
+    private final File directory = TestHelper.getDirectory(getClass());
+
+    @Test
+    @Timeout(3)
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, directory, "sample");
+    }
+
+    @Test
+    @Timeout(3)
+    public void brianfry713() throws Exception {
+        TestHelper.run(Main::main, directory, "brianfry713");
+    }
+
+    @Test
+    @Timeout(3)
+    public void jddantes() throws Exception {
+        TestHelper.run(Main::main, directory, "jddantes");
+    }
+
+    @Test
+    @Timeout(3)
+    public void rizaldi() throws Exception {
+        TestHelper.run(Main::main, directory, "rizaldi");
+    }
+}
