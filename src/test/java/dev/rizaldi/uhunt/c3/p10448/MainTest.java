a2d51ee2e74c8bcd869bd9375c59ee8c3e94175c
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2025-10-08 17:11:10 +0700

    [AC] 10448 - Unique World

---
+++ src/test/java/dev/rizaldi/uhunt/c3/p10448/MainTest.java
@@ -0,0 +1,35 @@
+package dev.rizaldi.uhunt.c3.p10448;
+
+import dev.rizaldi.uhunt.helper.TestHelper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.File;
+
+public class MainTest {
+    private final File directory = TestHelper.getDirectory(getClass());
+
+    @Test
+    @Timeout(3)
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, directory, "sample");
+    }
+
+    @Test
+    @Timeout(3)
+    public void starswap() throws Exception {
+        TestHelper.run(Main::main, directory, "starswap_1", "starswap_2");
+    }
+
+    @Test
+    @Timeout(3)
+    public void uva() throws Exception {
+        TestHelper.run(Main::main, directory, "uva");
+    }
+
+    @Test
+    @Timeout(3)
+    public void razumnik() throws Exception {
+        TestHelper.run(Main::main, directory, "razumnik");
+    }
+}
