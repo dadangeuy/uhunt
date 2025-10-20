0a1d3fa882d443d74aecc5ed0409bfcce3608518
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2025-10-21 03:10:01 +0700

    [AC] 11254 - Consecutive Integers

---
+++ src/test/java/uhunt/c3/p11254/MainTest.java
@@ -0,0 +1,21 @@
+package uhunt.c3.p11254;
+
+import org.junit.jupiter.api.Test;
+import uhunt.helper.TestHelper;
+
+public class MainTest {
+    @Test
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, "sample");
+    }
+
+    @Test
+    public void uva() throws Exception {
+        TestHelper.run(Main::main, "uva");
+    }
+
+    @Test
+    public void feodorv() throws Exception {
+        TestHelper.run(Main::main, "feodorv_1", "feodorv_2");
+    }
+}
