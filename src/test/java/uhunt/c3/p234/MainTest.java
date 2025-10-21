cf99635a1694e9bd62de10a1ec102682b8b149cd
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2025-10-21 19:54:27 +0700

    [AC] 234 - Switching Channels

---
+++ src/test/java/uhunt/c3/p234/MainTest.java
@@ -0,0 +1,21 @@
+package uhunt.c3.p234;
+
+import org.junit.jupiter.api.Test;
+import uhunt.helper.TestHelper;
+
+public class MainTest {
+    @Test
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, "sample");
+    }
+
+    @Test
+    public void batman() throws Exception {
+        TestHelper.run(Main::main, "batman");
+    }
+
+    @Test
+    public void goodeath() throws Exception {
+        TestHelper.run(Main::main, "goodeath");
+    }
+}
