956727bfef3b188e6ec3299078510929e1765f08
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2025-11-03 19:35:55 +0700

    [AC] 10003 - Cutting Sticks

---
+++ src/test/java/uhunt/c3/p10003/MainTest.java
@@ -0,0 +1,21 @@
+package uhunt.c3.p10003;
+
+import org.junit.jupiter.api.Test;
+import uhunt.helper.TestHelper;
+
+public class MainTest {
+    @Test
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, "sample");
+    }
+
+    @Test
+    public void udebug() throws Exception {
+        TestHelper.run(Main::main, "udebug");
+    }
+
+    @Test
+    public void brianfry() throws Exception {
+        TestHelper.run(Main::main, "brianfry");
+    }
+}
