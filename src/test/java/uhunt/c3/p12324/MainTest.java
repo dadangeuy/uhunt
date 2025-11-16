0d226dee061a1982f25727d342618de2ac74dfc1
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2025-11-17 03:12:27 +0700

    [AC] 12324 - Philip J. Fry Problem

---
+++ src/test/java/uhunt/c3/p12324/MainTest.java
@@ -0,0 +1,20 @@
+package uhunt.c3.p12324;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import uhunt.helper.TestHelper;
+
+@Nested
+@Timeout(2)
+public class MainTest {
+    @Test
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, "sample");
+    }
+
+    @Test
+    public void anjupiter() throws Exception {
+        TestHelper.run(Main::main, "anjupiter");
+    }
+}
