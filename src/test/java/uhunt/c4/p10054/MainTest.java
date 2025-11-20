7e09df49f2c131eba080cf0f4e8ce7a59fad5eee
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2025-11-21 03:36:20 +0700

    [AC] 10054 - The Necklace

---
+++ src/test/java/uhunt/c4/p10054/MainTest.java
@@ -0,0 +1,43 @@
+package uhunt.c4.p10054;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import uhunt.helper.TestHelper;
+
+@Nested
+@Timeout(3)
+public class MainTest {
+    @Test
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, "sample");
+    }
+
+    @Test
+    public void pedro_1() throws Exception {
+        TestHelper.run(Main::main, "pedro_1");
+    }
+
+    @Test
+    @Disabled("accepted result, but output is non-deterministic")
+    public void pedro_2() throws Exception {
+        TestHelper.run(Main::main, "pedro_2");
+    }
+
+    @Test
+    @Disabled("accepted result, but output is non-deterministic")
+    public void brian() throws Exception {
+        TestHelper.run(Main::main, "brian");
+    }
+
+    @Test
+    public void psz_1() throws Exception {
+        TestHelper.run(Main::main, "psz_1");
+    }
+
+    @Test
+    public void psz_2() throws Exception {
+        TestHelper.run(Main::main, "psz_2");
+    }
+}
