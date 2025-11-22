55549aba2584daad82237cad244bb943b6967568
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2025-11-22 14:23:35 +0700

    [AC] 10263 - Railway

---
+++ src/test/java/uhunt/c7/p10263/MainTest.java
@@ -0,0 +1,20 @@
+package uhunt.c7.p10263;
+
+import org.junit.jupiter.api.Test;
+import uhunt.helper.TestHelper;
+
+import java.time.Duration;
+
+public class MainTest {
+    private static final Duration timeout = Duration.ofSeconds(3);
+
+    @Test
+    public void sample() throws Exception {
+        TestHelper.run(Main::main, "sample");
+    }
+
+    @Test
+    public void uva() throws Exception {
+        TestHelper.run(Main::main, "uva");
+    }
+}
