109f1f416af72487a015b9ee5b08e1156bd6d56a
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2026-01-20 17:35:00 +0700

    [AC] 11783 - Nails

---
+++ src/test/java/uhunt/c7/p11783/MainTest.java
@@ -0,0 +1,20 @@
+package uhunt.c7.p11783;
+
+import org.junit.jupiter.api.Test;
+import uhunt.helper.TestHelper;
+
+import java.time.Duration;
+
+public class MainTest {
+    private static Duration TIMEOUT = Duration.ofSeconds(1);
+
+    @Test
+    public void sample() throws Exception {
+        TestHelper.run(TIMEOUT, Main::main, "sample");
+    }
+
+    @Test
+    public void metaphysis() throws Exception {
+        TestHelper.run(TIMEOUT, Main::main, "metaphysis");
+    }
+}
