054d8d146a30f638d87d37b3f0c935da4ee4d4b2
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2026-01-23 04:18:47 +0700

    [AC] 13142 - Destroy the Moon to Save the Earth

---
+++ src/test/java/uhunt/c3/g2/p13142/MainTest.java
@@ -0,0 +1,15 @@
+package uhunt.c3.g2.p13142;
+
+import org.junit.jupiter.api.Test;
+import uhunt.helper.TestHelper;
+
+import java.time.Duration;
+
+public class MainTest {
+    private static final Duration TIMEOUT = Duration.ofSeconds(1);
+
+    @Test
+    public void sample() throws Exception {
+        TestHelper.run(TIMEOUT, Main::main, "sample");
+    }
+}
