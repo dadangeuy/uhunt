d4257b998459760682216d7eb80f35b440a68f0a
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2026-02-18 00:49:26 +0700

    [AC] 11838 - Come and Go

---
+++ src/test/java/uhunt/c4/g8/p11838/MainTest.java
@@ -0,0 +1,15 @@
+package uhunt.c4.g8.p11838;
+
+import org.junit.jupiter.api.Test;
+import uhunt.helper.TestHelper;
+
+import java.time.Duration;
+
+public class MainTest {
+    private static final Duration TIMEOUT = Duration.ofSeconds(2);
+
+    @Test
+    public void sample() throws Exception {
+        TestHelper.run(TIMEOUT, Main::main, "sample");
+    }
+}
