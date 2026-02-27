0146ccce1a3a6dd0fa422a245ae694e856442860
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2026-02-27 11:57:41 +0700

    Add graph

---
+++ src/main/java/uva/common/template/Graph.java
@@ -0,0 +1,33 @@
+package uva.common.template;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+@SuppressWarnings("unused")
+public final class Graph<V, E> {
+    public final Map<V, Map<V, E>> edges = new HashMap<>();
+
+    public void addBi(final V vertex1, final V vertex2, final E edge) {
+        addUni(vertex1, vertex2, edge);
+        addUni(vertex2, vertex1, edge);
+    }
+
+    public void addUni(final V fromVertex, final V intoVertex, final E edge) {
+        edges.computeIfAbsent(fromVertex, k -> new HashMap<>()).put(intoVertex, edge);
+    }
+
+    public Set<V> get() {
+        return edges.keySet();
+    }
+
+    public Set<V> get(final V fromVertex) {
+        return edges.getOrDefault(fromVertex, Collections.emptyMap()).keySet();
+    }
+
+    public Optional<E> get(final V fromVertex, final V intoVertex) {
+        return Optional.ofNullable(edges.getOrDefault(fromVertex, Collections.emptyMap()).get(intoVertex));
+    }
+}
